1080c30256268e651657f59e40703d75bcdaf5a4
iltoeeai <tadas.kuprinskas@gmail.com>
2021-01-28 09:00:52 +0200

    Olympic Game Task

---
+++ Olympic Game Task/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olympics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int beginning = 1896;
+            int end = 2021;
+
+            Console.WriteLine("Enter the first date: ");
+            a = int.Parse(Console.ReadLine());
+
+            if (a >= beginning & a <= end - 1)
+            {
+                Console.WriteLine("Enter the second date: ");
+                b = int.Parse(Console.ReadLine());
+                if (b >= beginning + 1 & b <= end)
+                {
+                    for(int i = a; i < b; i++)
+                    {
+                        for(int j = beginning; j < end; j += 4)
+                        {
+                            if (i == j)
+                            {
+                                if (j == 1916 || j == 1940 || j == 1944 || i == 2020)
+                                    continue;       
+                                
+                               Console.WriteLine(j);
+                            }                               
+                        }
+                    }
+                }
+                else
+                    Console.WriteLine("Wrong date entered!");
+            }
+            else
+                Console.WriteLine("First Olympic games were held in 1896!");           
+        }
+    }
+}
---
+++ Olympic Game Task/README.md
@@ -0,0 +1,11 @@
+# Olympic Game Task :white_check_mark:
+
+Olympic Games are being held since 1896. Since then they happen every 4 years except in 1916, 1940, 1944 and 2020 they were cancelled. The task is to enter to the console period of time to check the years when the Games
+were held.
+___
+
+### Screenshots
+
+<div>
+    <img src="screenshot.png"/>
+</div>
